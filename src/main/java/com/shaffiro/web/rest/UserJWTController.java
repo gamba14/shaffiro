80ca0e8d27597c0edc7197dc78f6fa89e1bf5243
Agustin <gambirassi.a@gmail.com>
2019-06-24 10:36:45 -0300

    Social media login

--- src/main/java/com/shaffiro/web/rest/UserJWTController.java
+++ src/main/java/com/shaffiro/web/rest/UserJWTController.java
@@ -24,48 +24,62 @@ import javax.validation.Valid;
 @RequestMapping("/api")
 public class UserJWTController {
 
-    private final TokenProvider tokenProvider;
+	private final TokenProvider tokenProvider;
 
-    private final AuthenticationManager authenticationManager;
+	private final AuthenticationManager authenticationManager;
 
-    public UserJWTController(TokenProvider tokenProvider, AuthenticationManager authenticationManager) {
-        this.tokenProvider = tokenProvider;
-        this.authenticationManager = authenticationManager;
-    }
+	public UserJWTController(TokenProvider tokenProvider, AuthenticationManager authenticationManager) {
+		this.tokenProvider = tokenProvider;
+		this.authenticationManager = authenticationManager;
+	}
 
-    @PostMapping("/authenticate")
-    public ResponseEntity<JWTToken> authorize(@Valid @RequestBody LoginVM loginVM) {
+	@PostMapping("/authenticate")
+	public ResponseEntity<JWTToken> authorize(@Valid @RequestBody LoginVM loginVM) {
 
-        UsernamePasswordAuthenticationToken authenticationToken =
-            new UsernamePasswordAuthenticationToken(loginVM.getUsername(), loginVM.getPassword());
+		UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+				loginVM.getUsername(), loginVM.getPassword());
 
-        Authentication authentication = this.authenticationManager.authenticate(authenticationToken);
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-        boolean rememberMe = (loginVM.isRememberMe() == null) ? false : loginVM.isRememberMe();
-        String jwt = tokenProvider.createToken(authentication, rememberMe);
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.add(JWTFilter.AUTHORIZATION_HEADER, "Bearer " + jwt);
-        return new ResponseEntity<>(new JWTToken(jwt), httpHeaders, HttpStatus.OK);
-    }
+		Authentication authentication = this.authenticationManager.authenticate(authenticationToken);
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+		boolean rememberMe = (loginVM.isRememberMe() == null) ? false : loginVM.isRememberMe();
+		String jwt = tokenProvider.createToken(authentication, rememberMe);
+		HttpHeaders httpHeaders = new HttpHeaders();
+		httpHeaders.add(JWTFilter.AUTHORIZATION_HEADER, "Bearer " + jwt);
+		return new ResponseEntity<>(new JWTToken(jwt), httpHeaders, HttpStatus.OK);
+	}
 
-    /**
-     * Object to return as body in JWT Authentication.
-     */
-    static class JWTToken {
+	@PostMapping("/authenticateSM")
+	public ResponseEntity<JWTToken> autorizeSM(@Valid @RequestBody LoginVM loginVM) {
+		UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+				loginVM.getUsername(), loginVM.getPassword());
 
-        private String idToken;
+		Authentication authentication = this.authenticationManager.authenticate(authenticationToken);
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+		boolean rememberMe = (loginVM.isRememberMe() == null) ? false : loginVM.isRememberMe();
+		String jwt = tokenProvider.createToken(authentication, rememberMe);
+		HttpHeaders httpHeaders = new HttpHeaders();
+		httpHeaders.add(JWTFilter.AUTHORIZATION_HEADER, "Bearer " + jwt);
+		return new ResponseEntity<>(new JWTToken(jwt), httpHeaders, HttpStatus.OK);
+	}
 
-        JWTToken(String idToken) {
-            this.idToken = idToken;
-        }
+	/**
+	 * Object to return as body in JWT Authentication.
+	 */
+	static class JWTToken {
 
-        @JsonProperty("id_token")
-        String getIdToken() {
-            return idToken;
-        }
+		private String idToken;
 
-        void setIdToken(String idToken) {
-            this.idToken = idToken;
-        }
-    }
+		JWTToken(String idToken) {
+			this.idToken = idToken;
+		}
+
+		@JsonProperty("id_token")
+		String getIdToken() {
+			return idToken;
+		}
+
+		void setIdToken(String idToken) {
+			this.idToken = idToken;
+		}
+	}
 }
